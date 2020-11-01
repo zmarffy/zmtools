d8eed2fb2b31aa4299c2c5cbdd245bf144a89216
Zeke Marffy <zmarffy@me.com>
2020-11-01 17:23:38 +0000

    add req

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setuptools.setup(
     long_description=open('README.txt').read(),
     long_description_content_type='text/plain',
     install_requires=[
-        "getch"
+        "getch",
+        "packaging"
     ],
 )
